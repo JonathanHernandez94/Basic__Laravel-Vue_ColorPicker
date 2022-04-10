4eb59fd4c16d8a55a9fc39e33fff635e3a6a2666
jestevezmma1994 <jestevezmma1994@gmail.com>
2022-04-10 02:26:36 -0300

    almost_done fronted

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,38 +4,79 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+
 const { method, split } = require('lodash');
+
 
 require('./bootstrap');
 
 window.Vue = require('vue').default;
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.prototype.$style;
+Vue.prototype.$direction;
+Vue.prototype.$syntax;
+Vue.prototype.$color1;
+Vue.prototype.$color2;
 const app = new Vue({
     el: '#app',
     data: {
-        container_right_output_input: 'Hi EEUU'
-
+        code: Vue.prototype.$syntax
     },
+
     methods: {
         myColor1(e) {
+            Vue.prototype.$color1 = e.target.value;
             this.$refs.color1.value = e.target.value;
+            this.renderCode();
         },
         myColor2(e) {
+            Vue.prototype.$color2 = e.target.value;
             this.$refs.color2.value = e.target.value;
+            this.renderCode();
         },
         hexToRgb() {
-            if (this.$refs.color1.value[0] !== '#' || this.$refs.color2.value[0] !== '#') {
+
+            $("#hex").removeClass("selected");
+            $("#rgb").removeClass("selected");
+            $("#rgb").addClass("selected");
+
+            if (this.$refs.color1.value[0] !== '#' && this.$refs.color2.value[0] !== '#') {
                 return;
+            } else if (this.$refs.color1.value[0] == '#' && this.$refs.color2.value[0] !== '#') {
+                this.$refs.color1.value = (['0x' + this.$refs.color1.value[1] + this.$refs.color1.value[2] | 0, '0x' + this.$refs.color1.value[3] + this.$refs.color1.value[4] | 0, '0x' + this.$refs.color1.value[5] + this.$refs.color1.value[6] | 0]);
+            } else if (this.$refs.color1.value[0] !== '#' && this.$refs.color2.value[0] == '#') {
+                this.$refs.color2.value = (['0x' + this.$refs.color2.value[1] + this.$refs.color2.value[2] | 0, '0x' + this.$refs.color2.value[3] + this.$refs.color2.value[4] | 0, '0x' + this.$refs.color2.value[5] + this.$refs.color2.value[6] | 0]);
             } else {
                 this.$refs.color1.value = (['0x' + this.$refs.color1.value[1] + this.$refs.color1.value[2] | 0, '0x' + this.$refs.color1.value[3] + this.$refs.color1.value[4] | 0, '0x' + this.$refs.color1.value[5] + this.$refs.color1.value[6] | 0]);
                 this.$refs.color2.value = (['0x' + this.$refs.color2.value[1] + this.$refs.color2.value[2] | 0, '0x' + this.$refs.color2.value[3] + this.$refs.color2.value[4] | 0, '0x' + this.$refs.color2.value[5] + this.$refs.color2.value[6] | 0]);
             }
+            Vue.prototype.$color1 = "rgb(" + this.$refs.color1.value + ")";
+            Vue.prototype.$color2 = "rgb(" + this.$refs.color2.value + ")";
+            this.renderCode();
         },
         rgbToHex() {
-            if (this.$refs.color1.value[0] == '#' || this.$refs.color2.value[0] == '#') {
+
+            $("#hex").removeClass("selected");
+            $("#rgb").removeClass("selected");
+            $("#hex").addClass("selected");
+
+            if (this.$refs.color1.value[0] == '#' && this.$refs.color2.value[0] == '#') {
                 return;
+            } else if (this.$refs.color1.value[0] !== '#' && this.$refs.color2.value[0] == '#') {
+                colors1 = this.$refs.color1.value.split(",");
+                red1 = colors1[0];
+                green1 = colors1[1];
+                blue1 = colors1[2];
+                const rgb1 = (red1 << 16) | (green1 << 8) | (blue1 << 0);
+                this.$refs.color1.value = '#' + (0x1000000 + rgb1).toString(16).slice(1);
+            } else if (this.$refs.color1.value[0] == '#' && this.$refs.color2.value[0] !== '#') {
+                colors2 = this.$refs.color2.value.split(",");
+                red2 = colors2[0];
+                green2 = colors2[1];
+                blue2 = colors2[2];
+                const rgb2 = (red2 << 16) | (green2 << 8) | (blue2 << 0);
+                this.$refs.color2.value = '#' + (0x1000000 + rgb2).toString(16).slice(1);
             } else {
                 colors1 = this.$refs.color1.value.split(",");
                 red1 = colors1[0];
@@ -51,6 +92,180 @@ const app = new Vue({
                 const rgb2 = (red2 << 16) | (green2 << 8) | (blue2 << 0);
                 this.$refs.color2.value = '#' + (0x1000000 + rgb2).toString(16).slice(1);
             }
+            Vue.prototype.$color1 = this.$refs.color1.value;
+            Vue.prototype.$color2 = this.$refs.color2.value;
+            this.renderCode();
+        },
+
+        renderStyle(e) {
+            $("#styleLinear").removeClass("selected");
+            $("#styleRadial").removeClass("selected");
+
+            switch (e.currentTarget.id) {
+                case 'styleLinear':
+                    $("#styleLinear").addClass("selected");
+                    this.renderCode('styleLinear');
+                    break;
+                case 'styleRadial':
+                    $("#styleRadial").addClass("selected");
+                    this.renderCode('styleRadial');
+                    break;
+                default:
+                    break;
+            }
+        },
+        renderDir(e) {
+            $("#top").removeClass("selected");
+            $("#topRight").removeClass("selected");
+            $("#right").removeClass("selected");
+            $("#bottomRight").removeClass("selected");
+            $("#bottom").removeClass("selected");
+            $("#bottomLeft").removeClass("selected");
+            $("#left").removeClass("selected");
+            $("#topLeft").removeClass("selected");
+            $("#center").removeClass("selected");
+
+            switch (e.currentTarget.id) {
+                case 'top':
+                    $("#top").addClass("selected");
+                    this.renderCode('to top');
+                    break;
+                case 'topRight':
+                    $("#topRight").addClass("selected");
+                    this.renderCode('to top right')
+                    break;
+                case 'right':
+                    $("#right").addClass("selected");
+                    this.renderCode('to right')
+                    break;
+                case 'bottomRight':
+                    $("#bottomRight").addClass("selected");
+                    this.renderCode('to bottom right')
+                    break;
+                case 'bottom':
+                    $("#bottom").addClass("selected");
+                    this.renderCode('to bottom')
+                    break;
+                case 'topLeft':
+                    $("#topLeft").addClass("selected");
+                    this.renderCode('to top left')
+                    break;
+                case 'left':
+                    $("#left").addClass("selected");
+                    this.renderCode('to left')
+                    break;
+                case 'bottomLeft':
+                    $("#bottomLeft").addClass("selected");
+                    this.renderCode('to bottom left')
+                    break;
+                case 'center':
+                    $("#center").addClass("selected");
+                    this.renderCode('center')
+                    break;
+                default:
+                    break;
+            }
+        },
+        renderCode(parm) {
+            color1 = this.$refs.color1.value;
+            Vue.prototype.$color1 = color1 ;
+            color2 = this.$refs.color2.value;
+            Vue.prototype.$color2 = color2 ; 
+            direction = 'to top';
+            Vue.prototype.$direction = direction;
+            //style = '';
+            if (parm == 'styleRadial') {
+                Vue.prototype.$style = 'radial-gradient';
+            } else if (parm == 'styleLinear') {
+                Vue.prototype.$style = 'linear-gradient';
+            } else if (parm != null) {
+                Vue.prototype.$direction = parm;
+            }
+
+            if (Vue.prototype.$style == 'radial-gradient') {
+                prefix = '-webkit-'
+                switch (this.$direction) {
+                    case 'to top':
+                        Vue.prototype.$direction = 'center top'
+                        break;
+                    case 'to top right':
+                        Vue.prototype.$direction = 'top right'
+                        break;
+                    case 'to right':
+                        Vue.prototype.$direction = 'right center'
+                        break;
+                    case 'to bottom right':
+                        Vue.prototype.$direction = 'bottom right'
+                        break;
+                    case 'to bottom':
+                        Vue.prototype.$direction = 'center bottom'
+                        break;
+                    case 'to top left':
+                        Vue.prototype.$direction = 'top left'
+                        break;
+                    case 'to left':
+                        Vue.prototype.$direction = 'left center'
+                        break;
+                    case 'to bottom left':
+                        Vue.prototype.$direction = 'bottom left'
+                        break;
+                    case 'center':
+                        Vue.prototype.$direction = 'center top';
+                        prefix = '';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (Vue.prototype.$style == 'linear-gradient') {
+                switch (this.$direction) {
+                    case 'center top':
+                        Vue.prototype.$direction = 'to top'
+                        break;
+                    case 'top right':
+                        Vue.prototype.$direction = 'to top right'
+                        break;
+                    case 'right center':
+                        Vue.prototype.$direction = 'to right'
+                        break;
+                    case 'bottom right':
+                        Vue.prototype.$direction = 'to bottom right'
+                        break;
+                    case 'center bottom':
+                        Vue.prototype.$direction = 'to bottom'
+                        break;
+                    case 'top left':
+                        Vue.prototype.$direction = 'to top left'
+                        break;
+                    case 'left center':
+                        Vue.prototype.$direction = 'to left'
+                        break;
+                    case 'bottom left':
+                        Vue.prototype.$direction = 'to bottom left'
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (this.$style == 'radial-gradient' && prefix != '') {
+                $("#center").css("display", "inline-block");
+                syntax = "background-image: " + prefix + this.$style + '(' + this.$direction + ',' + this.$color1 + ',' + this.$color2 + ')';
+            }else if (this.$style == 'radial-gradient' && prefix == ''){
+                $("#center").css("display", "inline-block");
+                syntax = "background-image: " + prefix + this.$style + '('+ this.$color1 + ',' + this.$color2 + ')';
+            }
+
+            else {
+                $("#center").css("display", "none");
+                Vue.prototype.$style == 'linear-gradient'
+                syntax = "background-image: " + this.$style + '(' + this.$direction + ',' + this.$color1 + ',' + this.$color2 + ')';
+            }
+
+            console.log(syntax);
+            this.$refs.screen.style = syntax;
+            this.$refs.code.value = syntax;
+
+
         }
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,87 +10,105 @@
 </head>
 
 <body class=" bg-neutral-800 ">
-    <div id="app" class=" ">
-        <div id="container" class=" bg-neutral-800  m-0 absolute  w-full h-full">
-            <div id="container_title" class=" p-8  relative flex flex-col min-h-min min-w-min">
+    <form method="POST" action="{{ route('gradient.store') }}">
+        @csrf
+        <div id="app" class=" ">
+            <div id="container" class=" bg-neutral-800  m-0 absolute  w-full h-full">
+                <div id="container_title" class=" p-8 relative flex flex-col min-h-min min-w-min">
 
-                <h1 id="container_title_h1" class=" font-sans font-medium  text-slate-100 text-2xl">CSS
-                    GRADIENT </h1>
-                <p id="container_title_p" class=" font-sans font-normal text-sm text-slate-400 font">Gradient
-                    Generator for
-                    Linear and Radial Css Color Gradients</p>
+                    <h1 id="container_title_h1" class=" font-sans font-medium  text-slate-100 text-2xl">CSS
+                        GRADIENT </h1>
+                    <p id="container_title_p" class=" font-sans font-normal text-sm text-slate-400 font">Gradient
+                        Generator for
+                        Linear and Radial Css Color Gradients</p>
 
-            </div>
-            <div id="container_input"
-                class=" bg-neutral-800  w-5/12 h-3/4  p-8 float-left flex flex-col min-h-min min-w-min ">
+                </div>
+                <div id="container_input"
+                    class=" bg-neutral-800  w-5/12 h-3/4  p-8 float-left flex flex-col min-h-min min-w-min ">
 
-                <div id="container_input_style" class=" py-4 flex flex-col min-h-min min-w-min">
-                    <span class=" buttons font-sans font-medium text-slate-100">
-                        <label for="conatiner_input_style"> STYLE: </label>
-                        <br>
-                        <a id="styleLinear" class=" button-1">Linear</a>
-                        <a id="styleRadial" class=" button-1">Radial</a>
-                    </span>
+                    <div id="container_input_style" class=" py-4 flex flex-col min-h-min min-w-min">
+                        <span class=" buttons font-sans font-medium text-slate-100">
+                            <label for="conatiner_input_style"> STYLE: </label>
+                            <br>
+                            <button id="styleLinear" class=" button-1 selected" @@click="renderStyle($event)"
+                                type="button">Linear</button>
+                            <button id="styleRadial" class=" button-1" @@click="renderStyle($event)"
+                                type="button">Radial</button>
+                        </span>
+                    </div>
+                    <div id="container_input_direction" class=" py-1 flex flex-col min-h-min min-w-min">
+                        <span class=" buttons font-sans font-medium text-slate-100">
+                            <label for="container_input_direction"> DIRECTION:</label>
+                            <br>
+                            <div id="container_input_direction_upper" class="   min-w-min">
+                                <button id="top" class=" button-1 m-1" @@click="renderDir($event)"
+                                    type="button">Top</button>
+                                <button id="topRight" class=" button-1 m-1" @@click="renderDir($event)"
+                                    type="button">Top right</button>
+                                <button id="right" class=" button-1 m-1" @@click="renderDir($event)"
+                                    type="button">Right</button>
+                                <button id="bottomRight" class=" button-1 m-1 " @@click="renderDir($event)"
+                                    type="button">Bottom right</button>
+                                <button id="center" class="button-1 m-1" @@click="renderDir($event)"
+                                    type="button" style="display: none">Center</button>
+                            </div>
+                            <div id="container_input_direction_lower" class="  ">
+                                <button id="bottom" class=" button-1 m-1 selected" @@click="renderDir($event)"
+                                    type="button">Bottom</button>
+                                <button id="bottomLeft" class=" button-1 m-1" @@click="renderDir($event)"
+                                    type="button">Bottom left</button>
+                                <button id="left" class=" button-1 m-1" @@click="renderDir($event)"
+                                    type="button">Left</button>
+                                <button id="topLeft" class=" button-1 m-1" @@click="renderDir($event)"
+                                    type="button">Top Left</button>
+                            </div>
+                        </span>
+                    </div>
+                    <div id="container_input_colors" class=" py-4 flex flex-col min-h-min min-w-min">
+                        <span class=" buttons font-sans font-medium text-slate-100">
+                            <label for="container_input_colors">COLORS:</label>
+                            <br>
+                            <div id="container_input_colors_upper" class="flex flex-row min-h-min min-w-min">
+                                <input id="container_input_colors_upper_color" type="color" name="colorUpper"
+                                    @@change="myColor1($event)" value="#00fbff" />
+                                <input id="container_input_colors_upper_text" type="text" name="colorUpperText" value="#05f1f5"
+                                    class="button-2" ref="color1" />
+                            </div>
+                            <div id="container_input_colors_lower" class="flex flex-row min-h-min min-w-min">
+                                <input id="container_input_colors_lower_color" type="color" name="colorLower"
+                                    @@change="myColor2($event)" value="#e1ffe0" />
+                                <input id="container_input_colors_lower_text" type="text" name="colorLowerText" value="#e2fee1"
+                                    class="button-2" ref="color2" />
+                            </div>
+                        </span>
+                    </div>
+                    <div id="container_input_format" class=" py-4 flex flex-col min-h-min min-w-min">
+                        <span class=" buttons font-sans font-medium text-slate-100">
+                            <label for="container_input_format">COLOR FORMAT:</label>
+                            <br>
+                            <button id="hex" class=" button-1 selected" @@click="rgbToHex" type="button">Hex</button>
+                            <button id="rgb" class=" button-1" @@click="hexToRgb" type="button">Rgb</button>
+                        </span>
+                    </div>
                 </div>
-                <div id="container_input_direction" class=" py-1 flex flex-col min-h-min min-w-min">
-                    <span class=" buttons font-sans font-medium text-slate-100">
-                        <label for="container_input_direction"> DIRECTION:</label>
-                        <br>
-                        <div id="container_input_direction_upper" class="   min-w-min">
-                            <a id="top" class=" button-1 m-1">Top</a>
-                            <a id="topRight" class=" button-1 m-1">Top right</a>
-                            <a id="right" class=" button-1 m-1">Right</a>
-                            <a id="bottomRight" class=" button-1 m-1">Bottom right</a>
-                        </div>
-                        <div id="container_input_direction_lower" class="  ">
-                            <a id="bottom" class=" button-1 m-1">Bottom</a>
-                            <a id="topLeft" class=" button-1 m-1">Bottom left</a>
-                            <a id="left" class=" button-1 m-1">Left</a>
-                            <a id="topLeft" class=" button-1 m-1">Top Left</a>
-                        </div>
-                    </span>
-                </div>
-                <div id="container_input_colors" class=" py-4 flex flex-col min-h-min min-w-min">
-                    <span class=" buttons font-sans font-medium text-slate-100">
-                        <label for="container_input_colors">COLORS:</label>
-                        <br>
-                        <div id="container_input_colors_upper" class="flex flex-row min-h-min min-w-min">
-                            <input id="container_input_colors_upper_color" type="color" name="colorUpper" @@change="myColor1($event)" :value="color1v" />
-                            <input id="container_input_colors_upper_text" type="text" name="colorUpperText" value="" class="button-2" ref="color1"/>
-                        </div>
-                        <div id="container_input_colors_lower" class="flex flex-row min-h-min min-w-min">
-                            <input id="container_input_colors_lower_color" type="color" name="colorLower" @@change="myColor2($event)" :value="color2v" />
-                            <input id="container_input_colors_lower_text" type="text" name="colorLowerText" value="" class="button-2" ref="color2"/>
-                        </div>
-                    </span>
-                </div>
-                <div id="container_input_format" class=" py-4 flex flex-col min-h-min min-w-min">
-                    <span class=" buttons font-sans font-medium text-slate-100">
-                        <label for="container_input_format">COLOR FORMAT:</label>
-                        <br>
-                        <a id="hex" class=" button-1" @@click="rgbToHex" >Hex</a>
-                        <a id="rgb" class=" button-1" @@click="hexToRgb" >Rgb</a>
-                    </span>
-                </div>
-            </div>
-            <div id="conatiner_right"
-                class=" relative h-3/4 w-7/12 float-right bg-neutral-800 flex flex-col min-h-min min-w-min ">
-                <div id='container_righ_screen flex flex-col min-h-min min-w-min'
-                    class=" bg-blue-800 rounded relative float-right mx-1 h-2/3 w-11/12">
-                </div>
-                <div id="conatainer_righ_output"
-                    class=" bg-neutral-800 relative float-right mx-1 h-2/3 w-11/12 flex flex-col min-h-min min-w-min">
-                    <!--input id="container_right_outpu_input" type="text" value="@{{ code }}"
-                        class=" rounded w-full h-1/5 my-3 relative p-3 buttons font-sans font-medium text-slate-100 bg-transparent border-2 border-slate-100" /-->
-                    <input id="container_right_output_input" type="text" :value="container_right_output_input"
-                        class=" rounded w-full h-1/5 my-3 relative p-3 buttons font-sans font-medium text-slate-100 bg-transparent border-2 border-slate-100" ref="code"/>
+                <div id="conatiner_right"
+                    class=" relative h-3/4 w-7/12 float-right bg-neutral-800 flex flex-col min-h-min min-w-min ">
+                    <div id='container_righ_screen flex flex-col min-h-min min-w-min'
+                        class="rounded relative float-right mx-1 h-2/3 w-11/12" ref="screen"
+                        style="background-image: linear-gradient(to bottom,#05f1f5,#e2fee1)">
+                    </div>
+                    <div id="conatainer_righ_output"
+                        class=" bg-neutral-800 relative float-right mx-1 h-2/3 w-11/12 flex flex-col min-h-min min-w-min">
+                        <input id="container_right_output_input" type="text" value="background-image: linear-gradient(to bottom,#05f1f5,#e2fee1)"
+                            class=" rounded w-full h-1/5 my-3 relative p-3 buttons font-sans font-medium text-slate-100 bg-transparent border-2 border-slate-100"
+                            name="title" ref="code" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
+    </form>
     <script src="{{ asset('js/app.js') }}">
-    
+
 
     </script>
 </body>
